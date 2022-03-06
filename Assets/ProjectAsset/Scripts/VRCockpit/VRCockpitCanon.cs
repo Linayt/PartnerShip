c44557fc29f9c6573064696d8d7c88777e88c031
Nuttyfree22 <74913132+Nuttyfree22@users.noreply.github.com>
2022-03-06 16:47:39 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/Linayt/PartnerShip""
    This reverts commit 4ea9ceddb4b5e3f47f53e4a7055212a8a393e5ed.

--- Assets/ProjectAsset/Scripts/VRCockpit/VRCockpitCanon.cs
+++ Assets/ProjectAsset/Scripts/VRCockpit/VRCockpitCanon.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class VRCockpitCanon : MonoBehaviour
 {
@@ -15,14 +16,19 @@ public class VRCockpitCanon : MonoBehaviour
 
     bool canFire;
     float cooldown;
+    bool isFiring;
 
     VRCockpitControler vrControler;
     public void Initialize()
     {
         vrControler = GetComponent<VRCockpitControler>();
         canFire = false;
+        isFiring = false;
         cooldown = cooldownBetweenFire;
-
+        XRIDefaultInputActions vrPlayerInput = new XRIDefaultInputActions();
+        vrPlayerInput.XRIRightHand.Enable();
+        vrPlayerInput.XRIRightHand.Fireinput.started += StartFire;
+        vrPlayerInput.XRIRightHand.Fireinput.canceled += StopFire;
     }
 
     
@@ -30,7 +36,7 @@ public class VRCockpitCanon : MonoBehaviour
     public void ChangeCanFireState(bool state) => canFire = state;
 
 
-    public bool CanFire() => canFire && cooldown == cooldownBetweenFire;
+    public bool CanFire() => canFire && cooldown == cooldownBetweenFire && isFiring;
 
     public void SpawnProjectile()
     {
@@ -42,6 +48,18 @@ public class VRCockpitCanon : MonoBehaviour
             }
             cooldown = 0;
         }
+    }
+
+    public void StartFire(InputAction.CallbackContext context)
+    { 
+        isFiring = true;
+        Debug.Log("StartFire");
+    }
+
+    public void StopFire(InputAction.CallbackContext context)
+    {
+        isFiring = false;
+        Debug.Log("StopFire");
     }
 
     public void ChangeCooldownValue() => cooldown = Mathf.Clamp(cooldown + Time.deltaTime, 0, cooldownBetweenFire);
